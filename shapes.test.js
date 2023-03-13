b90ef9d30e7af9e2359bb61a95fbdff526b7728f
Kyle Parks <107071647+Goobber33@users.noreply.github.com>
2023-03-13 13:51:19 -0700

    Finished adding the rest of the tests on the shapes for my test file.

--- shapes.test.js
+++ shapes.test.js
@@ -30,4 +30,31 @@ describe('generateSvg', () => {
     expect(svg).toMatch(`fill="${circleShape.bgColor}"`);
     expect(svg).toMatch(`stroke="${circleShape.textColor}"`);
   });
+
+  test('should generate a square logo svg', () => {
+    const squareShape = new SShape({ logoText: 'ABC', textColor: 'red', bgColor: 'white' });
+    const svg = generateSvg(squareShape);
+    expect(svg).toMatch('<rect');
+    expect(svg).toMatch('x="20" y="20" width="60" height="60"');
+    expect(svg).toMatch(`fill="${squareShape.bgColor}"`);
+    expect(svg).toMatch(`stroke="${squareShape.textColor}"`);
+  });
+
+  test('should generate a triangle logo svg', () => {
+    const triangleShape = new TShape({ logoText: 'ABC', textColor: 'red', bgColor: 'white' });
+    const svg = generateSvg(triangleShape);
+    expect(svg).toMatch('<polygon');
+    expect(svg).toMatch('points="50,10 90,90 10,90"');
+    expect(svg).toMatch(`fill="${triangleShape.bgColor}"`);
+    expect(svg).toMatch(`stroke="${triangleShape.textColor}"`);
+  });
+
+  test('should generate a diamond logo svg', () => {
+    const diamondShape = new DShape({ logoText: 'ABC', textColor: 'red', bgColor: 'white' });
+    const svg = generateSvg(diamondShape);
+    expect(svg).toMatch('<polygon');
+    expect(svg).toMatch('points="50,5 95,50 50,95 5,50"');
+    expect(svg).toMatch(`fill="${diamondShape.bgColor}"`);
+    expect(svg).toMatch(`stroke="${diamondShape.textColor}"`);
+  });
 });
